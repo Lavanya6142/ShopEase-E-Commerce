dcc15ac84c51494b2459fa2da463a07379cb6ea8
Lavanya6142 <lavanyak6142@gmail.com>
2026-09-16 19:51:21 +0530

    Allow GitHub Pages frontend in CORS

--- Backend/ecommerce/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ Backend/ecommerce/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -85,9 +85,12 @@ public class SecurityConfig {
         CorsConfiguration configuration =
                 new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-            List.of("http://127.0.0.1:5500")
-        );
+       configuration.setAllowedOrigins(
+    List.of(
+        "http://127.0.0.1:5500",
+        "https://lavanya6142.github.io"
+    )
+);
 
         configuration.setAllowedMethods(
             List.of(
